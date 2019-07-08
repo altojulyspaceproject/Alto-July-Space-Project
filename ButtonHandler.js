045d3cdf7963910945820880f714a815a228cf72
abdiaziz <abdiazizfarah5@gmail.com>
2019-07-08 04:18:27 +0100

    UserInput prototype complete

---
+++ ButtonHandler.js
@@ -0,0 +1,17 @@
+var UserName
+var Password;
+var NoradID;
+
+
+function ButtonHandler(username , upassword, noradid) {
+
+	UserName = username;
+	Password = upassword;
+	NoradID = noradid;
+	
+
+	console.log(UserName);
+	console.log(Password);
+	console.log(NoradID);
+
+}
