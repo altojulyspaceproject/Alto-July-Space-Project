6ea4319188730cf698c282b28d1b2334ce9c2b09
abdiaziz <abdiazizfarah5@gmail.com>
2019-07-08 05:19:37 +0100

    Version1

--- ButtonHandler.js
+++ ButtonHandler.js
@@ -15,3 +15,20 @@ function ButtonHandler(username , upassword, noradid) {
 	console.log(NoradID);
 
 }
+
+
+
+
+fetchTLEFromServer(noradID, username, password);
+
+//Set the interval to convert the current TLE for the ISS and repeat every 500ms
+setInterval(function(){
+  longLat = convertTLEtoCoordinates(tleLine1,tleLine2);
+  console.log(longLat);
+  // latArray.push(longLat["lat"]);
+  // longArray.push(longLat["long"]);
+
+  // console.log(latArray);
+  // console.log(longArray);
+
+},3000);
