5730341a8f615aa73ff952e6b53084ff8afb5d77
Adrian <adrian@matthews-mbp-3.mobility.unimelb.net.au>
2019-07-10 17:13:06 +1000

    this commit will aadd a tracker algorithm to python

--- tracker.py
+++ tracker.py
@@ -1,18 +1,20 @@
 import numpy as np
 from scipy import constants as cn
 
-#testing code - ask the user for object position (ECEF)
-print('Enter coordinates in ECEF (in kilometres)')
-x_coordinate = input('x coordinate:')
-y_coordinate = input('y coordinate:')
-z_coordinate = input('z coordinate:')
+#testing code - ask the user for ground station position, convert to radians
+print('Enter ground station coordinates in Latitude/longitude (in degrees)')
+ground_lat_coordinate = input('latitude coordinate:')*cn.pi/180
+ground_long_coordinate = input('longitude coordinate:')*cn.pi/180
+
+#testing code - ask the user for satellite position, convert to radians
+print('Enter satellite coordinates in Latitude/longitude (in degrees)')
+ground_lat_coordinate = input('latitude coordinate:')*cn.pi/180
+ground_long_coordinate = input('longitude coordinate:')*cn.pi/180
 
 #create the vector for object position
 object_pos = (x_coordinate, y_coordinate, z_coordinate)
 print(object_pos)
 
-print(cn.pi)
-
 #create antenna direction vector that is parallel to the receiving surface, initialised at correct  
 #position, 90 degrees (orthogonal) to the object direction
-antenna_pos = 0
+antenna_pos = (ground_lat_coordinate)
