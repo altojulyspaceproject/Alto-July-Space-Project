ef97eec557864f39f99bf90750c3a71c44b6e4a2
Craig Robinson <raggy1995@gmail.com>
2019-07-12 14:13:17 +1000

    Stable release for TLE data from NORAD

--- ButtonHandler.js
+++ ButtonHandler.js
@@ -9,26 +9,19 @@ function ButtonHandler(username , upassword, noradid) {
 	Password = upassword;
 	NoradID = noradid;
 	
-
-	console.log(UserName);
-	console.log(Password);
-	console.log(NoradID);
-
+	
+	fetchTLEFromServer(NoradID, UserName, Password);
+	
+	setInterval(function(){
+	  longLat = convertTLEtoCoordinates(tleLine1,tleLine2);
+	  update(longLat);
+	  console.log(longLat);
+	},3000);
+	
+	
+	
 }
 
 
 
 
-fetchTLEFromServer(noradID, username, password);
-
-//Set the interval to convert the current TLE for the ISS and repeat every 500ms
-setInterval(function(){
-  longLat = convertTLEtoCoordinates(tleLine1,tleLine2);
-  console.log(longLat);
-  // latArray.push(longLat["lat"]);
-  // longArray.push(longLat["long"]);
-
-  // console.log(latArray);
-  // console.log(longArray);
-
-},3000);
--- README.md
+++
@@ -1,2 +0,0 @@
-# Alto-July-Space-Project
-For the student software team working on the Mission Control System July 2019
