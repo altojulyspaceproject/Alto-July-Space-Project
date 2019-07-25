ffd5e02ed5962c0262f8cf9d031541b6981ec4cf
TheMapleTr3 <yanqiu@student.unimelb.edu.au>
2019-07-26 00:05:35 +1000

    redirect

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-# cd /home/ec2-user/project
+cd /home/ec2-user/project
+#this script is run in temp location, so need to cd to directory for pm2 to start
 pm2 start npm --name spaceApp -- start --log /tmp/SpaceApp.log
 # pm2 start npm -- start --name spaceApp --log /tmp/SpaceApp.log
