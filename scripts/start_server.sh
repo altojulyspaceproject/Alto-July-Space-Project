1e14c6dc388f99d1ca02b477ca1337a562e6aec8
TheMapleTr3 <yanqiu@student.unimelb.edu.au>
2019-07-25 23:38:20 +1000

    start_server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 # cd /home/ec2-user/project
-# pm2 start npm --name "spaceApp" -- start >> /tmp/SpaceApp.log
-pm2 start npm -- start --name spaceApp --log /tmp/SpaceApp.log
+pm2 start npm --name spaceApp -- start --log /tmp/SpaceApp.log
+# pm2 start npm -- start --name spaceApp --log /tmp/SpaceApp.log
