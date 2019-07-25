4a3f470385c785e34639133da58aae3d9fd21bc4
TheMapleTr3 <yanqiu@student.unimelb.edu.au>
2019-07-25 22:26:29 +1000

    added logs

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 now=$(date +"%m_%d_%Y")
-sudo mv /home/ec2-user/project /home/ec2-user/project_old_$now
+sudo mv /home/ec2-user/project /home/ec2-user/project_old_$now >> /tmp/SpaceApp.log
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/project
-pm2 start npm --name "spaceApp" -- start
+pm2 start npm --name "spaceApp" -- start >> /tmp/SpaceApp.log
