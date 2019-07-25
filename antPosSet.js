e9c7652cbda720ea180750039f228f5ce5c86cc8
stephaniebatten <stephaniestockbatten.1@gmail.com>
2019-07-25 10:01:46 +1000

    bug, possible way to run bash script

--- antPosSet.js
+++ antPosSet.js
@@ -1,3 +1,16 @@
 function antPosSet(ant,ele){
-    console.log("sh antPosSet.sh " + ant + " "+ ele);
+    var cmd = "sh antPosSet.sh";
+    var exec = require('child_process').exec;
+    var returned;
+
+    var child = exec(cmd, function(error, stdout, stderr){
+
+        console.log('stdout: ' + stdout);
+        returned = stdout;
+        
+      if(error !== null){
+        console.log('exec error: ' + error);
+      }
+
+    });
 }
