7a6655e16c95097cfa03c36820acf6f87e4f6cf4
Craig Robinson <raggy1995@gmail.com>
2019-07-25 11:40:16 +1000

    Some branch merge fails

--- ButtonHandler.js
+++ ButtonHandler.js
@@ -1,27 +1,3 @@
-
-<<<<<<< HEAD
-function ButtonHandler(username , password, noradID, groundLatitude, groundLongitude) {
-var refreshCounter=0;
-	fetchTLEFromServer(noradID, username, password);
-	setTimeout(function(){
-		plotReal(latlongHolder[0],latlongHolder[1],latlongHolder[2],latlongHolder[3]); // plot function to initiate the map
-	
-	},3000);
-	
-		setInterval(function(){
-		newLatLongPlotData(tleLine1,tleLine2) //retriveing new lat/long data for the map 
-
-		longLat = convertTLEtoCoordinates(tleLine1,tleLine2); // new data for the google maps 
-		update(longLat); // updates the google maps 
-
-		setTimeout(function(){ 
-		plotReal1(latlongHolder[0],latlongHolder[1],latlongHolder[2],latlongHolder[3]); // updating the plot 
-		},500);
-		},3000);
-}
-console.log(latlongHolder );
-=======
-  
   
   function buttonHandler(username , password, noradID) {
 
@@ -102,4 +78,3 @@ console.log(latlongHolder );
     }
   }
   
->>>>>>> origin/master
