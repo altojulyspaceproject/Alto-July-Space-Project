28a624c198c7bb89ad3460db0eba422f3a1ccc07
TheMapleTr3 <yanqiu@student.unimelb.edu.au>
2019-07-25 13:55:29 +1000

    dont knowwww

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-cd /home/ec2-user/project
+# cd /home/ec2-user/project
 npm i
-cd scripts/
-chmod +x start_server.sh
+# cd scripts/
+# chmod +x start_server.sh
